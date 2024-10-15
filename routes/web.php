3505db203a7063a547a4eb12a4ad3d97d3a610f8
difawitsqard <difawitsqard@gmail.com>
2024-10-15 10:46:45 +0700

    add route

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\dashboard\DashboardController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\dashboard\superadmin\OutletManagementController;
 use App\Http\Controllers\dashboard\superadmin\StockItemManagementController;
+use App\Http\Controllers\dashboard\UnitManagementController;
 
 Auth::routes();
 
@@ -14,13 +16,25 @@ Route::middleware(['auth'])->group(function () {
 
   // Superadmin
   Route::group(['middleware' => ['role:superadmin']], function () {
-    Route::prefix('outlet/{outlet}')->group(function () {
-      Route::resource('stock-item-management', StockItemManagementController::class);
-    });
 
-    Route::resource('outlet', OutletManagementController::class);
+    Route::resource('unit', UnitManagementController::class)->only(['index', 'store', 'update', 'destroy']);
+    Route::get('unit/{id}/fetch', [UnitManagementController::class, 'fetch'])->name('unit.fetch');
+
+    Route::resource('outlet', OutletManagementController::class)->only(['index', 'store', 'update', 'destroy']);
+    Route::get('outlet/{id}/fetch', [OutletManagementController::class, 'fetch'])->name('outlet.fetch');
+    Route::get('outlet/{outlet:slug}', [DashboardController::class, 'index'])->name('outlet.dashboard');
+    Route::prefix('outlet/{outlet:slug}')->name('outlet.')->group(function () {
+      Route::resource('stock-item', StockItemManagementController::class);
+      Route::get('stock-item/{id}/fetch', [StockItemManagementController::class, 'fetch'])->name('stock-item.fetch');
+      Route::put('stock-item/{id}/restock', [StockItemManagementController::class, 'restock'])->name('stock-item.restock');
+    });
     // Route::prefix('dashboard')->name('dashboard.')->group(function () {
     //   Route::resource('outlet-management', OutletManagementController::class)->only(['index', 'show']);
     // });
+  });
+
+  // Admin
+  Route::group(['middleware' => ['role:admin']], function () {
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
   });
 });
