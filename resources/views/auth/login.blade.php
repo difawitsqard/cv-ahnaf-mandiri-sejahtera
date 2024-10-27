d89323014c83a830cdd8901bf692531512fafeb3
difawitsqard <difawitsqard@gmail.com>
2024-10-27 18:39:40 +0700

    Login view update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,52 +3,17 @@
     Login
 @endsection
 @section('content')
-    <div class="section-authentication-cover">
-        <div class="">
-            <div class="row g-0">
-                <div
-                    class="col-12 col-xl-7 col-xxl-8 auth-cover-left align-items-center justify-content-center d-none d-xl-flex border-end bg-transparent">
+    <!--authentication-->
+    <div class="auth-basic-wrapper d-flex align-items-center justify-content-center">
+        <div class="container-fluid my-5 my-lg-0">
+            <div class="row">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5 col-xxl-4 mx-auto">
+                    <div class="card rounded-4 mb-0 border-top border-4">
+                        <div class="card-body p-5">
+                            <h3 class="fw-bolder">{{ config('app.name') }} </h3>
+                            <p class="mb-0">Masukan email dan password anda untuk login</p>
 
-                    <div class="card rounded-0 mb-0 border-0 shadow-none bg-transparent bg-none">
-                        <div class="card-body">
-                            <img src="{{ URL::asset('build/images/auth/login1.png') }}" class="img-fluid auth-img-cover-login"
-                                width="650" alt="">
-                        </div>
-                    </div>
-
-                </div>
-
-                <div
-                    class="col-12 col-xl-5 col-xxl-4 auth-cover-right align-items-center justify-content-center border-top border-4 border-primary border-gradient-1">
-                    <div class="card rounded-0 m-3 mb-0 border-0 shadow-none bg-none">
-                        <div class="card-body p-sm-5">
-                            <img src="{{ URL::asset('build/images/logo1.png') }}" class="mb-4" width="145"
-                                alt="">
-                            <h4 class="fw-bold">Get Started Now</h4>
-                            <p class="mb-0">Enter your credentials to login your account</p>
-
-                            <div class="row g-3 my-4">
-                                <div class="col-12 col-lg-6">
-                                    <button
-                                        class="btn btn-light py-2 font-text1 fw-bold d-flex align-items-center justify-content-center w-100"><img
-                                            src="{{ URL::asset('build/images/apps/05.png') }}" width="20" class="me-2"
-                                            alt="">Google</button>
-                                </div>
-                                <div class="col col-lg-6">
-                                    <button
-                                        class="btn btn-light py-2 font-text1 fw-bold d-flex align-items-center justify-content-center w-100"><img
-                                            src="{{ URL::asset('build/images/apps/17.png') }}" width="20" class="me-2"
-                                            alt="">Facebook</button>
-                                </div>
-                            </div>
-
-                            <div class="separator section-padding">
-                                <div class="line"></div>
-                                <p class="mb-0 fw-bold">OR</p>
-                                <div class="line"></div>
-                            </div>
-
-                            <div class="form-body mt-4">
+                            <div class="form-body my-5">
                                 <form method="POST" action="{{ route('login') }}" class="row g-3">
                                     @csrf
 
@@ -56,9 +21,8 @@
                                         <label for="email" class="form-label">Email <span
                                                 class="text-danger">*</span></label>
                                         <input type="email" class="form-control @error('email') is-invalid @enderror"
-                                            id="email" name="email" placeholder="jhon@example.com" value="{{ old('email') }}"
-                                            required autocomplete="email" autofocus>
-
+                                            id="email" name="email" placeholder="Email"
+                                            value="{{ old('email') }}" required autocomplete="email" autofocus>
                                         @error('email')
                                             <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $message }}</strong>
@@ -82,32 +46,20 @@
                                             @enderror
                                         </div>
                                     </div>
-
                                     <div class="col-md-6">
                                         <div class="form-check form-switch">
                                             <input class="form-check-input" type="checkbox" id="checkbox"
                                                 {{ old('remember') ? 'checked' : '' }}>
-                                            <label class="form-check-label" for="checkbox">Remember Me</label>
+                                            <label class="form-check-label" for="checkbox">Ingat saya</label>
                                         </div>
                                     </div>
-
                                     @if (Route::has('password.request'))
-                                        <div class="col-md-6 text-end"> <a href="{{ route('password.request') }}">Forgot
-                                                Password ?</a>
+                                        <div class="col-md-6 text-end"> <a href="{{ route('password.request') }}">Lupa kata sandi ?</a>
                                         </div>
                                     @endif
-
                                     <div class="col-12">
                                         <div class="d-grid">
-                                            <button type="submit" class="btn btn-grd-primary">Login</button>
-                                        </div>
-                                    </div>
-
-                                    <div class="col-12">
-                                        <div class="text-start">
-                                            <p class="mb-0">Don't have an account yet? <a
-                                                    href="{{ route('register') }}">Sign Up</a>
-                                            </p>
+                                            <button type="submit" class="btn btn-primary">Login</button>
                                         </div>
                                     </div>
                                 </form>
@@ -116,9 +68,10 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div><!--end row-->
         </div>
     </div>
+    <!--authentication-->
 @endsection
 @push('script')
     <script>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,9 +5,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name') }} - @yield('title')</title>
+    <meta content="" name="description" />
+    <meta content="" name="keywords" />
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" href="{{ URL::asset('build/images/favicon-32x32.png') }}" type="image/png">
+
+    <!-- Favicons -->
+    <link href="{{ URL::asset('build/images/favicon.png') }}" rel="icon" />
+    <link href="{{ URL::asset('build/images/apple-touch-icon.png') }}" rel="apple-touch-icon" />
 
     @include('layouts.head-css')
 </head>
