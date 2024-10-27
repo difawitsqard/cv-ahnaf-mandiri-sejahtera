13417056d5b28eacd7b7fec1555247954f612ca4
difawitsqard <difawitsqard@gmail.com>
2024-10-27 16:16:31 +0700

    add roleBasedRoute

--- app/Helpers/global.php
+++ app/Helpers/global.php
@@ -6,3 +6,36 @@ if (!function_exists('formatRupiah')) {
     return number_format($amount, 0, ',', '.');
   }
 }
+
+if (! function_exists('roleBasedRoute')) {
+  function roleBasedRoute($name, $parameters = [])
+  {
+    /** @var \App\Models\User */
+    $user = auth()->user();
+    $prefix = '';
+
+    if ($user) {
+      if ($user->hasRole('superadmin')) {
+        $prefix = 'outlet.';
+      } elseif ($user->hasRole('admin')) {
+        $prefix = 'admin.';
+      } elseif ($user->hasRole('staff')) {
+        $prefix = 'staff.';
+      }
+    }
+
+    $routeName = $prefix . $name;
+    $route = \Route::getRoutes()->getByName($routeName);
+
+    if ($route && $route->parameterNames()) {
+      if ($user && $user->hasRole('admin|staff')) {
+        $parameters = array_filter($parameters, function ($key) {
+          return $key !== 'outlet';
+        }, ARRAY_FILTER_USE_KEY);
+      }
+      return route($routeName, $parameters);
+    } else {
+      return route($routeName);
+    }
+  }
+}
