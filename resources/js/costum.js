3b5dba3ae37e0c3d165c83fced5e0a99388a2605
difawitsqard <difawitsqard@gmail.com>
2024-12-03 08:18:33 +0700

    add pace costum

--- resources/js/costum.js
+++ resources/js/costum.js
@@ -1,79 +1,107 @@
 "use strict";
 
 function setEnable() {
-    // Enable all input elements except those with class 'ignore'
-    document.querySelectorAll("input:not(.ignore)").forEach(function (element) {
-        element.removeAttribute("disabled");
-    });
+    document.body.classList.remove("disable-pointer-events");
 
-    // Enable all select elements except those with class 'ignore'
+    // Enable all input elements except those with class 'not-disabled'
     document
-        .querySelectorAll("select:not(.ignore)")
+        .querySelectorAll("input:not(.not-disabled)")
+        .forEach(function (element) {
+            element.removeAttribute("readonly");
+        });
+
+    // Enable all select elements except those with class 'not-disabled'
+    document
+        .querySelectorAll("select:not(.not-disabled)")
         .forEach(function (element) {
             element.removeAttribute("disabled");
         });
 
-    // Enable all textarea elements except those with class 'ignore'
+    // Enable all textarea elements except those with class 'not-disabled'
     document
-        .querySelectorAll("textarea:not(.ignore)")
+        .querySelectorAll("textarea:not(.not-disabled)")
+        .forEach(function (element) {
+            element.removeAttribute("readonly");
+        });
+
+    // Enable all button elements except those with class 'not-disabled'
+    document
+        .querySelectorAll("button:not(.not-disabled)")
         .forEach(function (element) {
             element.removeAttribute("disabled");
         });
 
-    // Enable all button elements except those with class 'ignore'
+    // Remove 'disabled' class from all anchor elements except those with class 'not-disabled'
     document
-        .querySelectorAll("button:not(.ignore)")
+        .querySelectorAll("a:not(.not-disabled)")
         .forEach(function (element) {
-            element.removeAttribute("disabled");
+            element.classList.remove("disabled");
         });
-
-    // Remove 'disabled' class from all anchor elements except those with class 'ignore'
-    document.querySelectorAll("a:not(.ignore)").forEach(function (element) {
-        element.classList.remove("disabled");
-    });
-
-    // Remove 'btn-progress' class from all elements with class 'btn' except those with class 'ignore'
-    document.querySelectorAll(".btn:not(.ignore)").forEach(function (element) {
-        element.classList.remove("btn-progress");
-    });
 }
 
 function setDisable() {
-    // Disable all input elements except those with class 'ignore'
-    document.querySelectorAll("input:not(.ignore)").forEach(function (element) {
-        element.setAttribute("disabled", true);
-    });
+    document.body.classList.add("disable-pointer-events");
 
-    // Disable all select elements except those with class 'ignore'
+    // Disable all input elements except those with class 'not-disabled' and already disabled
     document
-        .querySelectorAll("select:not(.ignore)")
+        .querySelectorAll("input:not(.not-disabled)")
         .forEach(function (element) {
-            element.setAttribute("disabled", true);
+            if (
+                element.hasAttribute("readonly") ||
+                element.hasAttribute("disabled")
+            ) {
+                element.classList.add("not-disabled");
+            } else {
+                element.setAttribute("readonly", true);
+            }
         });
 
-    // Disable all textarea elements except those with class 'ignore'
+    // Disable all select elements except those with class 'not-disabled' and already disabled
     document
-        .querySelectorAll("textarea:not(.ignore)")
+        .querySelectorAll("select:not(.not-disabled)")
         .forEach(function (element) {
-            element.setAttribute("disabled", true);
+            if (element.hasAttribute("disabled")) {
+                element.classList.add("not-disabled");
+            } else {
+                element.setAttribute("disabled", true);
+            }
         });
 
-    // Disable all button elements except those with class 'ignore'
+    // Disable all textarea elements except those with class 'not-disabled' and already disabled
     document
-        .querySelectorAll("button:not(.ignore)")
+        .querySelectorAll("textarea:not(.not-disabled)")
         .forEach(function (element) {
-            element.setAttribute("disabled", true);
+            if (
+                element.hasAttribute("readonly") ||
+                element.hasAttribute("disabled")
+            ) {
+                element.classList.add("not-disabled");
+            } else {
+                element.setAttribute("readonly", true);
+            }
         });
 
-    // Add 'disabled' class to all anchor elements except those with class 'ignore'
-    document.querySelectorAll("a:not(.ignore)").forEach(function (element) {
-        element.classList.add("disabled");
-    });
+    // Disable all button elements except those with class 'not-disabled' and already disabled
+    document
+        .querySelectorAll("button:not(.not-disabled)")
+        .forEach(function (element) {
+            if (element.hasAttribute("disabled")) {
+                element.classList.add("not-disabled");
+            } else {
+                element.setAttribute("disabled", true);
+            }
+        });
 
-    // Add 'btn-progress' class to all elements with class 'btn' except those with class 'ignore'
-    document.querySelectorAll(".btn:not(.ignore)").forEach(function (element) {
-        element.classList.add("btn-progress");
-    });
+    // Add 'disabled' class to all anchor elements except those with class 'not-disabled'
+    document
+        .querySelectorAll("a:not(.not-disabled)")
+        .forEach(function (element) {
+            if (element.classList.contains("disabled")) {
+                element.classList.add("not-disabled");
+            } else {
+                element.classList.add("disabled");
+            }
+        });
 }
 
 function formatRupiahText(value) {
@@ -84,3 +112,30 @@ function formatRupiahText(value) {
 function formatRupiahElement(input) {
     input.value = formatRupiahText(input.value);
 }
+
+$(function () {
+    "use strict";
+
+    // pace costum
+    $(document).ajaxStart(function () {
+        Pace.restart();
+    });
+
+    Pace.on("start", function () {
+        setDisable();
+        $(".preloader").fadeIn();
+    });
+
+    Pace.on("done", function () {
+        $(".preloader").fadeOut(1000);
+        setEnable();
+    });
+
+    $("form").on("submit", function (event) {
+        if ($("body").attr("data-pace") === "true") {
+            if ($(this).attr("onSubmit") !== "return false") {
+                Pace.restart();
+            }
+        }
+    });
+});
